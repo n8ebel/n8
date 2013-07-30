b77bd9e4f22b2a453a63b9df0195333140b63c85
n8ebel <nebel@vandals.uidaho.edu>
2013-07-29 22:30:37 -0700

    #3 Added the drawable component stub files

---
+++ n8/Components/cDrawable_Component.cpp
@@ -0,0 +1,11 @@
+/*
+ * cDrawable_Component.cpp
+ * n8
+ *
+ * Author:          Nate Ebel
+ * Date:            7/29/13
+ * Organization:    n8Tech
+ *
+*/
+
+#include <iostream>
---
+++ n8/Components/cDrawable_Component.h
@@ -0,0 +1,16 @@
+/*
+ * cDrawable_Component.h
+ * n8
+ *
+ * Author:          Nate Ebel
+ * Date:            7/29/13
+ * Organization:    n8Tech
+ *
+ */
+
+#ifndef n8_cDrawable_Component_h
+#define n8_cDrawable_Component_h
+
+
+
+#endif
